59c19d7bed4d10103397e44851f80713406bd351
RahulRathod5181 <rahulrathod5181@gmail.com>
2023-06-29 01:27:07 +0530

    one-shayari

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ const port = 3000; // Change this to your desired port number
 app.get('/shayari/:word', async (req, res) => {
   const word = req.params.word;
   const shayari = await generateShayari(word);
-  res.send(shayari);
-  return
+  let firstShayari = shayari.split('\n\n')[0];
+  res.send(firstShayari);
 });
 
 async function generateShayari(word) {
@@ -36,6 +36,34 @@ async function generateShayari(word) {
 
   return response.data.choices[0].message.content;
 }
+
+// async function generateShayari(word) {
+//   const response = await axios.post(
+//     'https://api.openai.com/v1/engines/davinci-codex/completions',
+//     {
+//       prompt: `तुम कवि हो।\n${word}`,
+//       // max_tokens: 100,
+//       temperature: 0.7,
+//       top_p: 1,
+//       n: 1,
+//       stop: '\n',
+//       temperature: 0.7,
+//       frequency_penalty: 0.0,
+//       presence_penalty: 0.0,
+//       model: 'gpt-3.5-turbo',
+//       language: 'hi',
+//     },
+//     {
+//       headers: {
+//         'Content-Type': 'application/json',
+//         'Authorization': `Bearer ${process.env.API_KEY}`,
+//       },
+//     }
+//   );
+
+//   return response.data.choices[0].text.trim();
+// }
+
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
